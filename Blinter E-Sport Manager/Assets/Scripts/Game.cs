53f712eea84b909563e869f5e691b38e63b0478d
1Macho <ozjuanpa@gmail.com>
2015-08-07 12:10:15 -0500

    Alpha 1
    Login

---
+++ Blinter E-Sport Manager/Assets/Scripts/Game.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// Made by 1MachoKualquiera
+// Last update 12/7/15 
+
+public class Game : MonoBehaviour {
+	public GameObject screen;
+    public bool Gender;
+    public bool GenderSelected;
+	public float dampTime = 0.15f;
+	private Vector3 speed = Vector3.zero;
+    public int page; 
+	public Vector3 target;
+    public Image Male;
+    public Image Female;
+    public Color Marked;
+    public Color Dismarked;
+    public Button GenderNext;
+    public InputField NameBox;
+    public InputField SurnameBox;
+    public string Name;
+    public string Surname;
+    public Button NameNext;
+    public bool NameandSurname;
+    public Text SaveFoundText;
+    public int Money;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        smoothDamp();
+        RecalculatePage();
+        checkNameFields();
+	}
+
+	public void introacceptbutton ()
+	{
+        PlayerPrefs.SetString("AcceptedTerms", "True");
+        page = 1;
+        if (PlayerPrefs.HasKey("Name"))
+        {
+            page = -1;
+            SaveFoundText.text = "¡Parece que ya tienes una partida guardada!\nNombre: " + Name + Surname + "\nCréditos: " + Money;
+        }
+	}
+	public void saraintrobutton ()
+	{
+        page = 2;
+	}
+	public void male ()
+	{
+        Gender = true;
+        GenderSelected = true;
+        GenderOperations();
+	}
+	public void female ()
+	{
+        Gender = false;
+        GenderSelected = true;
+        GenderOperations();
+	}
+	public void sexselectionscreenforward ()
+	{
+        page = 3;
+	}
+	public void namebackward ()
+	{
+        page = 2;
+	}
+    public void checkNameFields ()
+    {
+        if (page == 3)
+        {
+            NameandSurname = NameBox.text == "" && SurnameBox.text == "";
+            if (!NameandSurname)
+            {
+                NameNext.interactable = true;
+            }
+        }
+    }
+	public void nameforward ()
+	{
+        Name = NameBox.text;
+        Surname = SurnameBox.text;
+        page = 4;
+        PlayerPrefs.SetString("Name", Name);
+        PlayerPrefs.SetString("Surname", Surname);
+    }
+
+    public void GenderOperations ()
+    {
+        if (Gender)
+        {
+            Male.color = Marked;
+            Female.color = Dismarked;
+            PlayerPrefs.SetInt("Gender", 1);
+        }
+        else
+        {
+            Female.color = Marked;
+            Male.color = Dismarked;
+            PlayerPrefs.SetInt("Gender", 0);
+        }
+        if (GenderSelected)
+        {
+            GenderNext.interactable = true;
+        }
+    }
+
+	public void smoothDamp ()
+	{
+        Vector3 destination = new Vector3(-target.x + 473.9999f, -target.y + 266.5003f);
+        transform.position = Vector3.SmoothDamp(transform.position, destination, ref speed, dampTime);
+    }
+
+    public Vector3 IndexToCoords (int index)
+    {
+        return new Vector3(index * 956, 0);
+    }
+    public void RecalculatePage ()
+    {
+        target = IndexToCoords(page);
+    }
+    public void AlreadyHasSaved ()
+    {
+        
+    }
+}
---
+++ Blinter E-Sport Manager/Assets/Scripts/Login.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour {
+    public InputField Usernamef;
+    public InputField Passwordf;
+    private string Username;
+    private string Password;
+    public WWW Server;
+    public Text Logtext;
+    public string Output;
+    public Image loadingContainer;
+    public Color Incorrect;
+    public Color Loading;
+    public Toggle Remember;
+    public Toggle Autologin;
+    // Use this for initialization
+	void Start () {
+        if(PlayerPrefs.HasKey("Remember"))
+        {
+            if (PlayerPrefs.GetInt("Remember") == 1)
+            {
+                Usernamef.text = PlayerPrefs.GetString("Username");
+                Passwordf.text = PlayerPrefs.GetString("Password");
+                Remember.isOn = true;
+            }
+        }
+        if (PlayerPrefs.HasKey("Autologin"))
+        { 
+            if (PlayerPrefs.GetInt("Autologin") == 1)
+            {
+                Autologin.isOn = true;
+                RunInput();
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Remember.isOn)
+        {
+            Autologin.interactable = true;
+        }
+        else
+        {
+            Autologin.interactable = false;
+            Autologin.isOn = false;
+        }
+	}
+    public void RunInput ()
+    {
+        StartCoroutine("InputData");
+    }
+    public IEnumerator InputData ()
+    {
+        Username = Usernamef.text;
+        Password = Passwordf.text;
+        Logtext.color = Loading;
+        Logtext.text = "Cargando...";
+        loadingContainer.enabled = true;
+        Server = new WWW("http://blinteruniverse.com/Alpha/BlinterManagerTeamLogin.php?login=" + Username +"&password=" + Password);
+        yield return Server;
+        loadingContainer.enabled = false;
+        Logtext.text = "";
+        Logtext.color = Incorrect;
+        Output = Server.text;
+        CheckData();
+        if (Remember.isOn)
+        {
+            PlayerPrefs.SetInt("Remember", 1);
+            PlayerPrefs.SetString("Username", Username);
+            PlayerPrefs.SetString("Password", Password);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Remeber", 0);
+            PlayerPrefs.DeleteKey("Username");
+            PlayerPrefs.DeleteKey("Password");
+        }
+        if (Autologin.isOn)
+        {
+            PlayerPrefs.SetInt("Autologin", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Autologin", 0);
+        }
+    }
+    public void CheckData()
+    {
+        if (Output == "1")
+        {
+            Logtext.text = "";
+            Application.LoadLevel("Expo");
+        }
+        else
+        {
+            Logtext.text = "Tu nombre de usuario o contraseña no son correctos";
+        }
+    }
+    public void ForgotPassword ()
+    {
+        Application.OpenURL("http://blinteruniverse.com/Alpha/my-account/lost-password/");
+    }
+    public void Register ()
+    {
+        Application.OpenURL("http://blinteruniverse.com/Alpha/registro/");
+    }
+    public void Facebook ()
+    {
+        Application.OpenURL("https://www.facebook.com/ViElectronicEntertainment");
+    }
+    public void Twitter ()
+    {
+        Application.OpenURL("https://twitter.com/BlinterUniverse");
+    }
+    public void Instagram ()
+    {
+        Application.OpenURL("https://instagram.com/velozasergio/");
+    }
+}
